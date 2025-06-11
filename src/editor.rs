2ed957cf004f073d567d7a47ed98041a153e4832
Jessica Cayaban <jest.cayaban@gmail.com>
2025-06-12 08:22:24 +0900

    Draws tildes

--- src/editor.rs
+++ src/editor.rs
@@ -9,17 +9,19 @@ pub struct Editor {
 
 impl Editor {
     pub fn default() -> Self {
-        Editor{should_quit: false}
+        Editor { should_quit: false }
     }
-    pub fn run(&mut self){
+    pub fn run(&mut self) {
         Self::initialize().unwrap();
         let result = self.repl();
         Self::terminate().unwrap();
         result.unwrap();
     }
+
     fn initialize() -> Result<(), std::io::Error> {
         enable_raw_mode()?;
-        Self::clear_screen()
+        Self::clear_screen()?;
+        Self::draw_rows()
     }
     fn terminate() -> Result<(), std::io::Error> {
         disable_raw_mode()
@@ -28,12 +30,21 @@ impl Editor {
         let mut stdout = stdout();
         execute!(stdout, Clear(ClearType::All))
     }
+    fn draw_rows() -> Result<(), std::io::Error> {
+        let (_, row) = crossterm::terminal::size()?;
+        for number in 0..row {
+            crossterm::cursor::MoveTo(0, number);
+            print!("~\r\n");
+        }
+        crossterm::cursor::MoveTo(0,1);
+        Ok(())
+    }
     fn repl(&mut self) -> Result<(), std::io::Error> {
         loop {
             let event = read()?;
             self.evaluate_event(&event);
             self.refresh_screen()?;
-            if self.should_quit{
+            if self.should_quit {
                 break;
             }
         }
@@ -42,26 +53,20 @@ impl Editor {
     fn evaluate_event(&mut self, event: &Event) {
         if let Key(KeyEvent {
             code, modifiers, ..
-
         }) = event
-            {
-                match code {
-                    Char('q') if *modifiers == KeyModifiers::CONTROL => {
-                        self.should_quit = true;
-
-                    }
-                    _ => (),
-
+        {
+            match code {
+                Char('q') if *modifiers == KeyModifiers::CONTROL => {
+                    self.should_quit = true;
                 }
-
+                _ => (),
             }
-
+        }
     }
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
         if self.should_quit {
             Self::clear_screen()?;
-            print!("Goodbye. \r\n");
-
+            print!("Goodbye.\r\n");
         }
         Ok(())
     }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,5 @@ mod editor;
 use editor::Editor;
 
 fn main() {
-    let editor=Editor::default();
-    print!("\x1b[2J");
     Editor::default().run();
 }
